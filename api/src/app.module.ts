7fddf4023e61d5babde03c694edbaa7e2388012d
tk1351 <takakikitsui0111@gmail.com>
2021-09-27 10:42:03 +0900

    chore: typeormによるDB連携設定

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
