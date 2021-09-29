a03cf2fd90d46b88b23c4f68547af91147ff8a5a
tk1351 <takakikitsui0111@gmail.com>
2021-09-29 17:35:01 +0900

    chore: Cookieを使うためcors 記述変更

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: process.env.APP_URL });
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080, '0.0.0.0');
 }
