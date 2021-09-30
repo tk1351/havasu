2cb69b1ef3a80b99a208f7d48c4acb9ad2a4e208
tk1351 <takakikitsui0111@gmail.com>
2021-09-30 20:49:33 +0900

    chore: API取得数やブログ著者のidを定義

--- app/src/utils/api/api.ts
+++ app/src/utils/api/api.ts
@@ -4,3 +4,7 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 })
+
+export const offset = 0
+export const limit = 10
+export const authorId = process.env.AUTHOR_ID
