26d92ec5832c29564428ea341a461a1346ae97fb
tk1351 <takakikitsui0111@gmail.com>
2021-10-01 16:52:18 +0900

    fix: ログインページにカスタムフック追加

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { NextPage } from 'next'
 import Login from '../components/auth/Login'
 import Navbar from '../components/common/Navbar'
+import { useRequireVisitor } from '../hooks/useRequireVisitor'
 
 type LoginPageProps = {}
 
 const login: NextPage<LoginPageProps> = () => {
+  useRequireVisitor()
   return (
     <>
       <Navbar />
