72b3789ee4575449cb5687ffe841ca13fb7c7ab6
tk1351 <takakikitsui0111@gmail.com>
2021-10-01 11:38:04 +0900

    fix: Mui v5 + emotion設定、初期マウント時にCookie経由で認証確認

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { CacheProvider, EmotionCache } from '@emotion/react'
+import { RecoilRoot, useSetRecoilState, useRecoilValue } from 'recoil'
 import createEmotionCache from '../src/createEmotionCache'
 import theme from '../src/theme'
+import Alert from '../components/common/Alert'
+import { currentUserState } from '../recoil/atoms/currentUser'
+import { fetchCurrentUser } from '../src/api/auth'
+import { isLoginState } from '../recoil/atoms/isLogin'
 
 const clientSideEmotionCache = createEmotionCache()
 
@@ -12,15 +18,36 @@ interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache
 }
 
+const AppInit = () => {
+  const setCurrentUser = useSetRecoilState(currentUserState)
+  const isLogin = useRecoilValue(isLoginState)
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const res = await fetchCurrentUser()
+        setCurrentUser(res.data)
+      } catch {
+        setCurrentUser(null)
+      }
+    })()
+  }, [isLogin])
+  return null
+}
+
 const MyApp = (props: MyAppProps) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <RecoilRoot>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+          <Alert />
+          <AppInit />
+        </ThemeProvider>
+      </CacheProvider>
+    </RecoilRoot>
   )
 }
 export default MyApp
