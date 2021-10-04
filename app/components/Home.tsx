2986e077a5b97cee80fb14d58d30498ca9300915
tk1351 <takakikitsui0111@gmail.com>
2021-10-04 18:32:02 +0900

    fix: HomeへCSS適用

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { IPost } from '../src/types/post'
 import PostItem from './posts/PostItem'
+import { Grid } from '@mui/material'
 
 type HomeProps = {
   posts: IPost[]
@@ -8,14 +9,18 @@ type HomeProps = {
 
 const Home: FC<HomeProps> = ({ posts }) => {
   return (
-    <div>
-      <h1>Home</h1>
-      <div>
-        {posts.map((post) => (
-          <PostItem key={post.id} post={post} />
-        ))}
-      </div>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <div>
+          {posts.map((post) => (
+            <PostItem key={post.id} post={post} />
+          ))}
+        </div>
+      </Grid>
+      <Grid item xs={4}>
+        Category
+      </Grid>
+    </Grid>
   )
 }
 
