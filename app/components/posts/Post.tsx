d87c6a0c41279f042d4b9e29e6bed94b93e80d3b
tk1351 <takakikitsui0111@gmail.com>
2021-10-04 18:44:33 +0900

    fix: Postのデザイン修正

--- app/components/posts/Post.tsx
+++ app/components/posts/Post.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { Grid } from '@mui/material'
 import { IPost } from '../../src/types/post'
 import { useUtcToZonedTime } from '../../hooks/useUtcToZonedTime'
 import PostContent from './PostContent'
@@ -12,16 +13,21 @@ const Post: FC<PostProps> = ({ post }) => {
   const { title, content, tags, createdAt } = post
   const { formatDate } = useUtcToZonedTime(createdAt)
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{formatDate}</p>
-      <div>
-        <PostTags tags={tags} />
-      </div>
-      <div>
-        <PostContent content={content} />
-      </div>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <h1>{title}</h1>
+        <p>{formatDate}</p>
+        <div>
+          <PostTags tags={tags} />
+        </div>
+        <div>
+          <PostContent content={content} />
+        </div>
+      </Grid>
+      <Grid item xs={4}>
+        Category
+      </Grid>
+    </Grid>
   )
 }
 
