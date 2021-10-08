1b031e254a8cd4f476194e8c25d60000b4547896
tk1351 <takakikitsui0111@gmail.com>
2021-10-09 05:48:16 +0900

    feat: タグを基準にpostを受け取る関数

--- app/src/api/post.ts
+++ app/src/api/post.ts
@@ -1,6 +1,14 @@
 import { PostInputs } from '../types/input'
-import api from './api'
+import api, { authorId, offset, limit } from './api'
+import { IPost } from '../types/post'
 
 export const registerPost = async (data: PostInputs) => {
   return await api.post<boolean>('/posts/create', data)
+}
+
+export const fetchPosts = async (tag: string) => {
+  const url = `/posts/${authorId}?offset=${offset}&limit=${limit}&tag=${encodeURI(
+    tag
+  )}`
+  return await api.get<[IPost[], number]>(url)
 }
