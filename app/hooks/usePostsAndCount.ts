9a5055cc0681f21e4accf0535c3b79b7cdec8fb3
tk1351 <takakikitsui0111@gmail.com>
2021-10-11 07:52:30 +0900

    fix: カスタムフックの返り値の変数名修正

--- app/hooks/usePostsAndCount.ts
+++ app/hooks/usePostsAndCount.ts
@@ -1,34 +1,36 @@
 import { useState, useEffect } from 'react'
+import { useRecoilState } from 'recoil'
 import { IPost } from '../src/types/post'
 import { fetchPostsByTag, fetchSearchResult } from '../src/api/post'
+import { postsState } from '../recoil/atoms/posts'
 
 type UsePostsAndCountReturnType = {
-  posts: IPost[]
-  count: number
+  data: IPost[]
+  postCount: number
 }
 
 export const usePostsAndCount = (
   category: 'tag' | 'search',
   query: string
 ): UsePostsAndCountReturnType => {
-  const [posts, setPosts] = useState<IPost[]>([])
-  const [count, setCount] = useState<number>(0)
+  const [data, setData] = useRecoilState<IPost[]>(postsState)
+  const [postCount, setPostCount] = useState<number>(0)
 
   useEffect(() => {
     ;(async () => {
       if (category === 'tag') {
-        const res = await fetchPostsByTag(query)
-        setPosts(res.data[0])
-        setCount(res.data[1])
+        const res = await fetchPostsByTag(query, 1)
+        setData(res.data[0])
+        setPostCount(res.data[1])
       }
 
       if (category === 'search') {
-        const res = await fetchSearchResult(query)
-        setPosts(res.data[0])
-        setCount(res.data[1])
+        const res = await fetchSearchResult(query, 1)
+        setData(res.data[0])
+        setPostCount(res.data[1])
       }
     })()
   }, [category, query])
 
-  return { posts, count }
+  return { data, postCount }
 }
